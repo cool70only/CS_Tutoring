ac771f9fd5da74a2b87d59d8be0f2d8000f0e913
cool70only <30752260+cool70only@users.noreply.github.com>
2017-08-05 10:24:11 -0400

    Added to beads.java
    Added basic USACO program structure

--- src/beads.java
+++ src/beads.java
@@ -1,4 +1,23 @@
+/*
+ID: liangiv12
+LANG: JAVA
+TASK: test
+*/
+import java.io.*;
+import java.util.*;
 
 public class beads {
-
+  public static void main (String [] args) throws IOException {
+    // Use BufferedReader rather than RandomAccessFile; it's much faster
+    BufferedReader f = new BufferedReader(new FileReader("beads.in"));
+                                                  // input file name goes above
+    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("beads.out")));
+    // Use StringTokenizer vs. readLine/split -- lots faster
+    StringTokenizer st = new StringTokenizer(f.readLine());
+						  // Get line, break into tokens
+    int i1 = Integer.parseInt(st.nextToken());    // first integer
+    int i2 = Integer.parseInt(st.nextToken());    // second integer
+    out.println(i1+i2);                           // output result
+    out.close();                                  // close the output file
+  }
 }
